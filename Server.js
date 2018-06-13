ff18455d07d04ad393bf19ba9b49b46dc92bd69e
medhach1 <medha.chakravarthula@ggktech.com>
2018-06-13 16:04:34 +0530

    first commit

---
+++ Server.js
@@ -0,0 +1,41 @@
+var express=require('express');
+var app=express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+let port=process.env.port||3030
+
+
+app.get('/',function(req,res,next){
+    res.send('Home');
+
+});
+
+ app.get('/chat', function(req, res, next) {
+  	res.sendFile(__dirname + '/public/chat.html');
+  });
+
+ app.use(express.static('public'));
+
+ 
+
+app.get('/Home',(req,res)=>{
+
+       res.send('Chat app HomePage!');
+})
+
+ io.on('connection', function(client) {
+  	console.log('Client connected...');
+
+  	client.on('join', function(data) {
+  		console.log(data);
+  	});
+
+      client.on('messages', function(data){
+  		client.emit('thread', data);
+  		client.broadcast.emit('thread', data);
+  	});
+
+
+  });
+
+server.listen(port);
