d39b6e50e70ab0042ad4a5d7d9280c1511f8781b
medhach1 <medha.chakravarthula@ggktech.com>
2018-06-13 16:34:14 +0530

    4 commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express=require('express');
 var app=express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-let port=process.env.port||3030
+let port=process.env.PORT||3030
 
 
 app.get('/',function(req,res,next){
